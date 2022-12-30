f5446e4e0d0606102324d1b93bf0197eb1d5aa6f
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-12-29 21:06:13 -0500

    method for sorter

--- src/cpt/sorter.java
+++ src/cpt/sorter.java
@@ -1,5 +1,13 @@
 package cpt;
+import java.io.BufferedReader; 
+import java.io.FileNotFoundException; 
+import java.io.FileReader; 
+import java.io.IOException; 
 
 public class sorter {
-    
+    public static void main(String [] arg){
+        String line = "";
+        String space = ",";
+        
+    }
 }
