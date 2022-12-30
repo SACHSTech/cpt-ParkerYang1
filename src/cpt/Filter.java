43bdcb2759accdbf469186402e640da84211873a
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-12-29 23:38:57 -0500

    comments for filter

--- src/cpt/Filter.java
+++ src/cpt/Filter.java
@@ -10,6 +10,7 @@ import java.io.PrintWriter;
 
 public class Filter {
     public static void main(String [] args) throws IOException{
+        // Buffer reader and writer 
         BufferedReader deaths = new BufferedReader(new FileReader("src/cpt/DataSet.csv"));
         PrintWriter sortedDeaths = new PrintWriter(new FileWriter("src/cpt/SortedDataSet.csv"));
 
@@ -17,17 +18,23 @@ public class Filter {
 
         line = deaths.readLine();
 
+        // While loop to go through the file 
         while(line != null){
             ArrayList<String> data = new ArrayList<>(Arrays.asList(line.split(",")));
             data.add(line);
+
+            // Writes data for the year of 2016 in a new CSV file 
             if(data.get(2).equalsIgnoreCase("2016")){
                 sortedDeaths.println(data.get(0) + "," + data.get(2) + "," + data.get(3));
+            
+            // Writes data for the year of 2017 in a new CSV file 
             } else if(data.get(2).equalsIgnoreCase("2017")){
                 sortedDeaths.println(data.get(0) + "," + data.get(2) + "," + data.get(3));
             }
             line = deaths.readLine();
         }
 
+        // Close
         sortedDeaths.close();
         deaths.close();
 
