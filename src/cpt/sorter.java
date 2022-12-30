69a0092ec33918535fdde7fefb462830938cc098
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-12-29 22:54:07 -0500

    finished sorter + added sorted data

--- src/cpt/sorter.java
+++ src/cpt/sorter.java
@@ -1,13 +1,35 @@
 package cpt;
+import java.util.*;
+import java.io.*;
 import java.io.BufferedReader; 
 import java.io.FileNotFoundException; 
-import java.io.FileReader; 
-import java.io.IOException; 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter; 
 
 public class sorter {
-    public static void main(String [] arg){
-        String line = "";
-        String space = ",";
-        
+    public static void main(String [] args) throws IOException{
+        BufferedReader deaths = new BufferedReader(new FileReader("src/cpt/dataset.csv"));
+        PrintWriter sortedDeaths = new PrintWriter(new FileWriter("src/cpt/sorteddataset.csv"));
+
+        String line; 
+
+        line = deaths.readLine();
+
+        while(line != null){
+            ArrayList<String> data = new ArrayList<>(Arrays.asList(line.split(",")));
+            data.add(line);
+            if(data.get(2).equalsIgnoreCase("2016")){
+                sortedDeaths.println(data.get(0) + "," + data.get(2) + "," + data.get(3));
+            } else if(data.get(2).equalsIgnoreCase("2017")){
+                sortedDeaths.println(data.get(0) + "," + data.get(2) + "," + data.get(3));
+            }
+            line = deaths.readLine();
+        }
+
+        sortedDeaths.close();
+        deaths.close();
+
     }
 }
