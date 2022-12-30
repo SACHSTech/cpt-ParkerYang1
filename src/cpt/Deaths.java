510e465d87b14cf2c97731f2940e584317a389fa
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-12-29 23:31:45 -0500

    Constructor for deaths

--- src/cpt/Deaths.java
+++ src/cpt/Deaths.java
@@ -1,5 +1,30 @@
 package cpt;
 
+/**
+ * Data Interaction and Visualization CPT 
+ * @author: P. Yang
+ * 
+ */
+
 public class Deaths {
-    
+
+    // Instance variables 
+    private String country;
+    private int year;
+    private int deaths; 
+
+    /**
+     * Constructor for deaths 
+     * @param countryName Name of the country
+     * @param yearOccured Recorded data for that year
+     * @param numDeaths Number of deaths 
+     * @author: P. Yang
+     * 
+     */
+    public Deaths(String countryName, int yearOccured, int numDeaths){
+        country = countryName;
+        year = yearOccured;
+        deaths = numDeaths; 
+
+    }
 }
