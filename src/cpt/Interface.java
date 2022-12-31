5b02a0a4f1a92bf7d66972065a911db5c5cabc14
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-12-31 17:14:32 -0500

    bar chart start

--- src/cpt/Interface.java
+++ src/cpt/Interface.java
@@ -15,29 +15,62 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.stage.Stage;
 
-public class Interface {
+public class Interface extends Application {
+    public static void main(String arg[]){
+        launch(arg);
+    }
+ 
+        private BarChart chart;
+        private CategoryAxis xAxis;
+        private NumberAxis yAxis;
 
-    public static void main(String args[]) throws IOException{
-        String line; 
-        String country;
-        
-        List<String> countryName = new ArrayList<String>();
+        public Parent BarCharts() throws IOException{
+            String line; 
+            String country;
+            int count = 0;
+            
+            List<String> countryName = new ArrayList<String>();
 
-        BufferedReader br = new BufferedReader(new FileReader("src/cpt/SortedDataSet.csv"));
-        line = br.readLine();
-
-        for(int i = 0; i < 457; i++){
+            BufferedReader br = new BufferedReader(new FileReader("src/cpt/SortedDataSet.csv"));
             line = br.readLine();
 
-            String[] splitLine = line.split(",");
-            country = splitLine[0];
+            for(int i = 0; i < 457; i++){
+                line = br.readLine();
 
-            if(i % 2 == 0){
-                countryName.add(country);
+                String[] splitLine = line.split(",");
+                country = splitLine[0];
+
+                if(i % 2 == 0){
+                    countryName.add(country);
+                }
             }
+
+            String[] newCountryName = new String[countryName.size()];
+            newCountryName = countryName.toArray(newCountryName);
+
+            br.close();
+
+            int size = newCountryName.length;
+            for(int i = 0; i < size; i++){
+                count++;
+            }
+
+            xAxis = new CategoryAxis();
+            xAxis.setCategories(FXCollections.<String>observableArrayList(newCountryName));
+            yAxis = new NumberAxis("Deaths", 0.0d, 5000.0d, 100.0d);
+            ObservableList<BarChart.Series> barChartData =
+            FXCollections.observableArrayList(
+                new BarChart.Series("2016", FXCollections.observableArrayList(
+                    new BarChart.Data(newCountryName[0], 567d)))
+            );
+            chart = new BarChart(xAxis, yAxis, barChartData, 1.0d);
+            return chart;
         }
 
-        System.out.println(countryName);
-        br.close();
-    } 
+        @Override public void start(Stage primaryStage) throws Exception {
+            primaryStage.setScene(new Scene(BarCharts()));
+            primaryStage.show();
+        }
+        
 }
+
