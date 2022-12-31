8c0c1ef66feb397a30fac590999a9a3302f5f892
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2022-12-31 02:34:33 -0500

    add country to array list

--- src/cpt/Interface.java
+++ src/cpt/Interface.java
@@ -5,31 +5,39 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException; 
 import java.io.FileReader;
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.stage.Stage;
 
 public class Interface {
 
     public static void main(String args[]) throws IOException{
         String line; 
         String country;
-        String year;
-        String deaths; 
+        
+        List<String> countryName = new ArrayList<String>();
 
         BufferedReader br = new BufferedReader(new FileReader("src/cpt/SortedDataSet.csv"));
+        line = br.readLine();
 
-        for (int i = 0; i < 458; i++){
+        for(int i = 0; i < 457; i++){
             line = br.readLine();
 
             String[] splitLine = line.split(",");
             country = splitLine[0];
-            year = splitLine[1];
-            deaths = splitLine[2];
 
-            double dblDeaths = Double.parseDouble(deaths);
-            int intYears = Integer.parseInt(year);
-
-            Deaths dead = new Deaths(country, intYears, dblDeaths);
-            System.out.println(dead);
+            if(i % 2 == 0){
+                countryName.add(country);
+            }
         }
+
+        System.out.println(countryName);
         br.close();
     } 
 }
