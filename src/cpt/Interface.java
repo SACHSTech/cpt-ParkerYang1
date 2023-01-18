b8276f628af5875c43799df084cd27880ddfe19a
ParkerYang1 <parker.yang23@ycdsbk12.ca>
2023-01-18 02:29:45 -0500

    semi-working line graph

--- src/cpt/Interface.java
+++ src/cpt/Interface.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
@@ -21,16 +22,24 @@ import javafx.scene.layout.VBox;
 
 public class Interface extends Application{
 
-    private static final String[] data2016 = { "Alpha", "Beta", "RC1", "RC2", "1.0", "1.1" };
     private LineChart<String, Number> chart;
     private CategoryAxis xAxis;
     private NumberAxis yAxis;
 
-    public Parent LineChart() {
+    Filter masterList = new Filter();
+
+    public Parent LineChart() throws IOException{
 
         xAxis = new CategoryAxis();
         yAxis = new NumberAxis();
         chart = new LineChart<>(xAxis, yAxis);
+
+        masterList.initializeList();
+        ArrayList<Deaths> list2016 = new ArrayList<>();
+        list2016 = masterList.yearSpecificList(2016);
+
+        ArrayList<Deaths> list2017 = new ArrayList<>();
+        list2017 = masterList.yearSpecificList(2017);
 
         // setup chart
         chart.setTitle("Death Rates From Mental Health and Substance Use Disorders");
@@ -41,26 +50,34 @@ public class Interface extends Application{
         XYChart.Series<String, Number> series2016 = new XYChart.Series<>();
         series2016.setName("2016");
 
-        for(int i = 0; i < 6; i++){
-        series2016.getData().add(new XYChart.Data<String, Number>(data2016[i], i));
+        for(int i = 0; i < list2016.size(); i++){
+        series2016.getData().add(new XYChart.Data<String, Number>(list2016.get(i).getCountry(), list2016.get(i).getDeaths()));
         chart.getData().add(series2016);
         }
 
         XYChart.Series<String, Number> series2017 = new XYChart.Series<>();
-        series2017.setName("2017");
-        series2017.getData().add(new XYChart.Data<String, Number>(data2016[0], 10d));
-        series2017.getData().add(new XYChart.Data<String, Number>(data2016[1], 20d));
-        series2017.getData().add(new XYChart.Data<String, Number>(data2016[2], 30d));
-        series2017.getData().add(new XYChart.Data<String, Number>(data2016[3], 40d));
-        series2017.getData().add(new XYChart.Data<String, Number>(data2016[4], 50d));
-        series2017.getData().add(new XYChart.Data<String, Number>(data2016[5], 60d));
-        chart.getData().add(series2017);
+        series2016.setName("2017");
+
+        for(int j = 0; j < list2017.size(); j++){
+            series2017.getData().add(new XYChart.Data<String, Number>(list2017.get(j).getCountry(), list2017.get(j).getDeaths()));
+            chart.getData().add(series2017);
+            }
         return chart;
     }
 
         @Override public void start(Stage primaryStage) throws Exception {
+            /* 
+            CheckBox box2016 = new CheckBox("2016");
+            CheckBox box2017 = new CheckBox("2017");
+
+            VBox layout = new VBox();
+            layout.setPadding(new Insets(20, 20, 20, 20));
+            layout.getChildren().addAll(box2016, box2017);
+            */
+
             primaryStage.setScene(new Scene(LineChart()));
             primaryStage.show();
+
         }
         public static void main(String[] args) {
             launch(args);
